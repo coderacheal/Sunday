de12fdffefa5d5e1b30d37a1de46c775ccb88d11
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-10-05 12:18:19 +0000

    Delete src folder in server sub-directory

--- API/main.py
+++ API/main.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI
+import uvicorn
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Sentiment(BaseModel):
+    text  = str
 
 @app.get("/")
 def home():
     return {'Greeting' : 'Hello'}
 
-
 @app.post("/predict_sentiment")
-def predict_sentiment():
+def predict_sentiment(data: Sentiment):
     pass
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="127.0.0.1", port=7860, reload=True)
