00b671d91ccad1cb2f1452b3718c8d4f786c783f
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-10-07 20:41:05 +0000

    Add requirements.txt file for the API

--- API/main.py
+++ API/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 import uvicorn
 from pydantic import BaseModel
+import joblib
 
 app = FastAPI()
 
+mobilebert = joblib.load('mobile_bert.joblib')
+
 class Sentiment(BaseModel):
-    text  = str
+    text: str
 
 @app.get("/")
 def home():
@@ -13,7 +16,11 @@ def home():
 
 @app.post("/predict_sentiment")
 def predict_sentiment(data: Sentiment):
-    pass
+    data = data.dict()
+    text = data['text']
+
+    prediction  = mobilebert.predict([[text]])
+    return {'prediction': prediction[0]}
 
 if __name__ == '__main__':
     uvicorn.run(app, host="127.0.0.1", port=7860, reload=True)
