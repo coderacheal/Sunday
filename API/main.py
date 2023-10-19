df80988602810143cd1849045e1c079e11784319
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-10-19 17:45:28 +0000

    Api working with general tokenizer which will be replaced with the tokenizer I used to to train the model, and save the label_encoder

--- API/main.py
+++ API/main.py
@@ -1,37 +1,45 @@
 from fastapi import FastAPI
-import uvicorn
 import torch
 from transformers import MobileBertTokenizer, MobileBertForSequenceClassification
+import joblib
+import uvicorn
 
 app = FastAPI()
 
-# Load the pre-trained model
-model_path = "/model/sunday_mobile_bert_model"
+# Load the my further trained model, tokenizer, label_encoder
+model_path = ".\model\sunday_mobile_bert_model"
 model = MobileBertForSequenceClassification.from_pretrained(model_path)
-tokenizer = MobileBertTokenizer.from_pretrained(model_path)
-
+tokenizer = MobileBertTokenizer.from_pretrained('google/mobilebert-uncased')
+# label_encoder = joblib.load(".\model\sunday_mobile_bert_model\label_encoder.joblib")
 
 @app.get("/")
 def home():
-    return {'Greeting' : 'Hello'}
+    return {'Health Check' : 'Status Ok'}
 
 @app.get("/predict/{text}")
-def predict(text: str):
+def predict_sentiment(text: str):
 
     # Tokenize the input text
     input_encoding = tokenizer(text, truncation=True, padding=True, max_length=512, return_tensors='pt')
 
-    # Forward pass through the model
-    with torch.no_grad():
-        model.eval()
-       
-        input_ids = input_encoding['input_ids']
-        attention_mask = input_encoding['attention_mask']
-        
-        outputs = model(input_ids, attention_mask=attention_mask)
+    # Extract input_id and attention_mask from encoding
+    input_ids = input_encoding['input_ids']
+    attention_mask = input_encoding['attention_mask']
+    
+    # Pass input_ids and attention mask for prediction
+    outputs = model(input_ids, attention_mask=attention_mask)
 
-        # Get predicted class and probability
-        predicted_class = torch.argmax(outputs.logits, dim=1)
-        predicted_probability = torch.softmax(outputs.logits, dim=1)
+    # Get predicted class and probability
+    predicted_class = torch.argmax(outputs.logits, dim=1).item()
+    predicted_probability = torch.softmax(outputs.logits, dim=1).tolist()
 
-        return {"predicted_class": predicted_class, "predicted_probability": predicted_probability}
+    # Extract probability for the predicted class only and inverse the prediction into text
+    class_probability = predicted_probability[0][predicted_class]
+    # predicted_label = label_encoder.inverse_transform([predicted_class])[0]
+    # predicted_probability = probabilities[0, predictions[0]].item()
+
+    return {"predicted_class": predicted_class, "predicted_probability": class_probability}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
+    
